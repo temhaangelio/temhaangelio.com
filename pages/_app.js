61943f739f78c67b9c5cad5c2e9ebb4422014661
Ahmet Yılmaz <temha@Ahmet-MacBook-Pro.local>
2024-02-25 16:35:03 +0300

    son

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
