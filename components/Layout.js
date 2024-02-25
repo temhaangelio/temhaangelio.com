ad869cbc9dfa162ec5bfc91ae2ab7c3029707fd4
Ahmet Yılmaz <temha@Ahmet-MacBook-Pro.local>
2024-02-25 16:32:13 +0300

    son

--- components/Layout.js
+++ components/Layout.js
@@ -24,6 +24,24 @@ export default function Layout(props) {
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <nav className="flex flex-row items-center justify-between pt-10">
         <span className="text-3xl font-bold">temha angelio</span>
